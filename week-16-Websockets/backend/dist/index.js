ca8ed804b4ea5e784fc32d4ad75aa3b9c3d3b64c
BeastxD7 <shashankdevadiga2003@gmail.com>
2025-01-22 00:05:04 +0530

    Added Room Logic

--- week-16-Websockets/backend/dist/index.js
+++ week-16-Websockets/backend/dist/index.js
@@ -4,10 +4,37 @@ const ws_1 = require("ws");
 const wss = new ws_1.WebSocketServer({ port: 8080 });
 const allSockets = [];
 wss.on("connection", (socket) => {
-    allSockets.push(socket);
-    socket.on("message", (message) => {
-        allSockets.forEach((socket) => {
-            socket.send("message: " + message);
+    try {
+        socket.on("message", (message) => {
+            const parsedmessage = JSON.parse(message.toString());
+            console.log(parsedmessage);
+            if (parsedmessage.type == "join") {
+                allSockets.push({
+                    roomId: parsedmessage.payload.roomId,
+                    socket,
+                });
+                socket.send("room joined: " + parsedmessage.payload.roomId);
+            }
+            let currentRoomId;
+            if (parsedmessage.type == "chat") {
+                const parsedmessage = JSON.parse(message.toString());
+                console.log(parsedmessage.payload.message);
+                if (parsedmessage.type == "chat") {
+                    for (let i = 0; i < allSockets.length; i++) {
+                        if (allSockets[i].socket == socket) {
+                            currentRoomId = allSockets[i].roomId;
+                        }
+                    }
+                }
+            }
+            for (let i = 0; i < allSockets.length; i++) {
+                if (allSockets[i].roomId === currentRoomId) {
+                    allSockets[i].socket.send(parsedmessage.payload.message);
+                }
+            }
         });
-    });
+    }
+    catch (error) {
+        socket.send(error);
+    }
 });
--- week-16-Websockets/backend/src/index.ts
+++ week-16-Websockets/backend/src/index.ts
@@ -2,14 +2,50 @@ import WebSocket, { WebSocketServer } from "ws";
 
 const wss = new WebSocketServer({ port: 8080 });
 
-const allSockets: WebSocket[] = [];
+interface User {
+  roomId: string;
+  socket: WebSocket;
+}
+
+const allSockets: User[] = [];
 
 wss.on("connection", (socket) => {
-  allSockets.push(socket);
+  try {
+    socket.on("message", (message) => {
+      const parsedmessage = JSON.parse(message.toString());
+      console.log(parsedmessage);
 
-  socket.on("message", (message) => {
-    allSockets.forEach((socket) => {
-      socket.send("message: " + message);
+      if (parsedmessage.type == "join") {
+        allSockets.push({
+          roomId: parsedmessage.payload.roomId,
+          socket,
+        });
+
+        socket.send("room joined: " + parsedmessage.payload.roomId);
+      }
+
+      let currentRoomId;
+
+      if (parsedmessage.type == "chat") {
+        const parsedmessage = JSON.parse(message.toString());
+        console.log(parsedmessage.payload.message);
+
+        if (parsedmessage.type == "chat") {
+          for (let i = 0; i < allSockets.length; i++) {
+            if (allSockets[i].socket == socket) {
+              currentRoomId = allSockets[i].roomId;
+            }
+          }
+        }
+      }
+
+      for (let i = 0; i < allSockets.length; i++) {
+        if (allSockets[i].roomId === currentRoomId) {
+          allSockets[i].socket.send(parsedmessage.payload.message);
+        }
+      }
     });
-  });
+  } catch (error) {
+    socket.send(error as string);
+  }
 });
