dccdf8c68020cf36ef44932d1bcf28af3df4a17f
BeastxD7 <shashankdevadiga2003@gmail.com>
2025-01-27 00:25:51 +0530

    Postgres Queries

--- week-17-postgres/dist/index.js
+++ week-17-postgres/dist/index.js
@@ -8,14 +8,58 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const pg_1 = require("pg");
-const pgClient = new pg_1.Client("postgresql://neondb_owner:npg_USg0d4BTznIt@ep-shiny-glade-a8ruezaf-pooler.eastus2.azure.neon.tech/neondb?sslmode=require");
-function main() {
+const express_1 = __importDefault(require("express"));
+const dotenv_1 = __importDefault(require("dotenv"));
+dotenv_1.default.config();
+const app = (0, express_1.default)();
+app.use(express_1.default.json());
+const client = new pg_1.Client(process.env.POSTGRES_URI);
+function connectDb() {
     return __awaiter(this, void 0, void 0, function* () {
-        yield pgClient.connect();
-        const response = yield pgClient.query("SELECT * FROM playing_with_neon;");
-        console.log(response);
+        try {
+            var temp = yield client.connect();
+            console.log("db connected!");
+        }
+        catch (error) {
+            console.log(error);
+        }
     });
 }
-main();
+connectDb();
+app.get("/users", function fetchUser(req, res) {
+    return __awaiter(this, void 0, void 0, function* () {
+        try {
+            var users = yield client.query("SELECT * FROM USERS;");
+            res.json({
+                users: users.rows,
+            });
+        }
+        catch (error) {
+            console.log(error);
+        }
+    });
+});
+app.post("/users", function InsertUser(req, res) {
+    return __awaiter(this, void 0, void 0, function* () {
+        try {
+            const username = req.body.username;
+            const user = yield client.query(`INSERT INTO USERS(NAME)VALUES('${username}');`);
+            res.status(200).json({
+                message: "User Inserted Succesfully!",
+            });
+        }
+        catch (error) {
+            console.log(error);
+            res.status(400).json({
+                message: "Error Inserting User.",
+                error
+            });
+        }
+    });
+});
+app.listen(3000);
--- week-17-postgres/src/index.ts
+++ week-17-postgres/src/index.ts
@@ -1,16 +1,50 @@
-import {Client} from 'pg';
+import { Client } from "pg";
+import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
 
+const app = express();
+app.use(express.json());
 
+const client = new Client(
+  process.env.POSTGRES_URI
+);
 
-const pgClient = new Client( "postgresql://neondb_owner:npg_USg0d4BTznIt@ep-shiny-glade-a8ruezaf-pooler.eastus2.azure.neon.tech/neondb?sslmode=require")
-
-
-async function main ()  {
-    await pgClient.connect();
-
-    const response = await pgClient.query("SELECT * FROM playing_with_neon;")
-    console.log(response);
+async function connectDb() {
+  try {
+    var temp = await client.connect();
+    console.log("db connected!");
+  } catch (error) {
+    console.log(error);
+  }
 }
+connectDb();
 
-main()
+app.get("/users", async function fetchUser(req, res) {
+  try {
+    var users = await client.query("SELECT * FROM USERS;");
+    res.json({
+      users: users.rows,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+});
 
+app.post("/users", async function InsertUser(req, res) {
+  try {
+    const username = req.body.username;
+    const user = await client.query(`INSERT INTO USERS(NAME)VALUES('${username}');`);
+    res.status(200).json({
+        message:"User Inserted Succesfully!",
+    })
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({
+        message: "Error Inserting User.",
+        error
+    })
+  }
+});
+
+app.listen(3000);
